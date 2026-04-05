35fcdc7fff3f867c431ae68555630827782e4b2a
Mantti123 <117097286+Mantti123@users.noreply.github.com>
2026-04-05 21:11:52 +0300

    Task I1: React rebuild in Docker (All-codes)

---
+++ All-codes/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+function NavBar() {
+  return (
+    <div className="navbar">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Reserve appointment</a></li>
+        <li><a href="#">Story</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
